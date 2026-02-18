bfc8eb7211c65ba75fd5608ff043ad7600e4a825
leonardo98914-create <leonardo98914@gmail.com>
2026-02-18 16:42:32 -0600

    Primera Practica

---
+++ tipos-de-dato.js
@@ -0,0 +1,42 @@
+// Ejemplos 
+
+let ejem1 = 42;
+let ejem2 = 'Veinticinco';
+let ejem3 = -666;
+let ejem4 = true;
+let ejem5 = 0;
+let ejem6 = ''; 
+let ejem7 = null; 
+let ejem8;
+let ejem9 = 'FALSE';
+
+
+
+console.log(typeof ejem1);
+console.log(ejem1);
+
+console.log(typeof ejem3);
+console.log(ejem3);
+
+console.log(typeof ejem5);
+console.log(ejem5);
+
+console.log(typeof ejem4);
+console.log(ejem4);
+
+console.log(typeof ejem9);
+console.log(ejem9);
+
+console.log(typeof ejem6);
+console.log(ejem6);
+
+console.log(typeof ejem8);
+console.log(ejem8);
+
+console.log(typeof ejem2);
+console.log(ejem2);
+
+console.log(typeof ejem7);
+console.log(ejem7);
+
+
